064b40b40f553ba6ace47afdfb1e045ce52b1aa1
Guest <guest-RQQQ3t@meet.mit.edu>
2013-10-30 19:12:03 +0200

    finished exercize a

--- Lab2/Divisors.py
+++ Lab2/Divisors.py
@@ -1,10 +1,10 @@
-if __name__ == "__main__":
-	
-	n = int(raw_input("enter a number: "))
+def divisors(n):
 	x = 1
 	while (x<n):
 		if (n%x==0):		
 			print x
 		x+=1
 	
-	
+if __name__ == "__main__":
+	n = int(raw_input("enter a number: "))	
+	divisors(n)
