f2c17cb8f548de1a583554532ec4bb6536934e9d
Guest <guest-RQQQ3t@meet.mit.edu>
2013-10-30 19:47:23 +0200

    finished exer c num 3

--- Lab2/randomness.py
+++ Lab2/randomness.py
@@ -1,13 +1,21 @@
 from random import randint
 def CoinFlip(n):
-	randomNum = randint(1,10)
+	randomNum = randint(0,10)
 	if (n=="n"):
 		if (randomNum%2==0):
 			return "H"
 		else:
 			return "T"
+def CoinFlip2(n,prob):
+	randomNum = randint(0,100)
+	if (n=="n"):
+		if (randomNum<=prob):
+			return "H"
+		else:
+			return "T"
 
 if __name__ == "__main__":
+	prob = int(raw_input("what is the probability of heads?"))
 	n = raw_input("press n to flip")
-	print CoinFlip(n)
+	print CoinFlip2(n,prob)
 		
